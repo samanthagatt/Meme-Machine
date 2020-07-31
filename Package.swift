b3649bab9bec55adfca38115fd4351f00a3e3e44
Samantha Gatt <smfgatt@gmail.com>
2020-07-31 12:19:33 -0400

    Sets up project

--- Package.swift
+++ Package.swift
@@ -6,9 +6,15 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git",
+                 .upToNextMinor(from: "3.0.0")),
+        // Swift interface for libgd graphics library
+        .package(url: "https://github.com/twostraws/SwiftGD.git",
+                 .upToNextMinor(from: "2.5.0")),
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "App", dependencies: ["Vapor", "Leaf", "SwiftGD"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
